ceffdfd1d86a8c570bceabb5bce18ca912a22fb9
Andrea Chiarelli <andrea.chiarelli@auth0.com>
2020-03-11 11:23:23 +0100

    Adding PasswordValidator project

---
+++ unit-tests/PasswordValidator/PasswordValidator.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Validators.Password
+{
+  public class PasswordValidator
+  {
+    public bool IsValid(string password)
+    {
+      Regex passwordPolicyExpression = new Regex(@"((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#!$%]).{8,20})");
+      return passwordPolicyExpression.IsMatch(password);
+    }
+  }
+}
